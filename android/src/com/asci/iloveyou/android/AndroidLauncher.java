f66ec1d87987b47bc170dca9ee28e5b00e3689ae
Kamil I. Segebre <kamil@unitec.edu>
2016-02-10 17:50:15 -0600

    First Commit

---
+++ android/src/com/asci/iloveyou/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.asci.iloveyou.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.asci.iloveyou.ILoveYou;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ILoveYou(), config);
+	}
+}
---
+++ desktop/src/com/asci/iloveyou/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.asci.iloveyou.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asci.iloveyou.ILoveYou;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ILoveYou(), config);
+	}
+}
